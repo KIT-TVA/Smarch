dba19f59714967f6e9ecd4b26397aa690328848d
Erik Hofmayer <ehofmayer@outlook.de>
2024-09-16 17:39:28 +0200

    build.py: Fail on errors

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+import shutil
 import subprocess
 
 # Fresh build of CnC and sharpSAT
-subprocess.run(["sh", "build.sh", "clean"], cwd="CnC")
-subprocess.run(["sh", "build.sh", "march_cu"], cwd="CnC")
+subprocess.run(["sh", "build.sh", "clean"], cwd="CnC").check_returncode()
+subprocess.run(["sh", "build.sh", "march_cu"], cwd="CnC").check_returncode()
 
-subprocess.run(["rm", "-r", "sharpSAT/build"], cwd=".")
-subprocess.run(["sh", "setupdev.sh"], cwd="sharpSAT")
-subprocess.run(["make", ], cwd="sharpSAT/build/Release")
+if Path("sharpSAT/build").exists():
+    shutil.rmtree("sharpSAT/build", ignore_errors=True)
+subprocess.run(["sh", "setupdev.sh"], cwd="sharpSAT").check_returncode()
+subprocess.run(["make", ], cwd="sharpSAT/build/Release").check_returncode()
